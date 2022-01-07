d1e9ec73428a06b7a90b142ba166460ae2787798
dreamersnme <34536053+dreamersnme@users.noreply.github.com>
2022-01-07 18:03:45 +0900

    Update runner.py

--- runner.py
+++ runner.py
@@ -5,13 +5,14 @@ import time
 import gym
 from torch.utils.tensorboard import SummaryWriter
 
+from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.noise import NormalActionNoise
-from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
+from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize, SubprocVecEnv
 import numpy as np
 
-env_name = 'Stacked-v0'
-entry = 'sim.env:StackedEnv'
 
+# <class 'stable_baselines3.td3.policies.TD3Policy'>
+# <class 'stable_baselines3.sac.policies.SACPolicy'>
 class TimeRecode:
     def __init__(self, writer, interval=2):
         self.total_sec=0
@@ -37,41 +38,54 @@ class TimeRecode:
 
 
 
-
 class IterRun:
 
     unit = 2050
     boost_step = 3* unit
     boosted = False
 
-    gradient_steps = 2
-    def __init__(self, MODEL, arc=[256, 128, 32]):
+    gradient_steps = 4
+    def __init__(self, MODEL, env_name, arc=[256, 128, 32], nproc=1):
+        self.env_name = env_name
         self.model_cls = MODEL
         self.name = MODEL.__name__
-        env = DummyVecEnv([lambda: gym.make(env_name, title=self.name, plot_dir="./sFig/{}".format(self.name))])
-        self.env = VecNormalize(env, norm_obs=True, norm_reward=True, clip_obs=10.)
+        self.test_env = DummyVecEnv([lambda: gym.make(env_name, title=self.name, plot_dir="./sFig/{}".format(self.name))])
+        self.env = self.make_env(nproc)
         self.writer = self.tensorboard("./summary_all/{}/".format(self.name))
         self.save = f"ckpt_{self.name}"
         self.buffer = None
         self.arch = arc
         self.iter = 1
+        self.train_start = time.time ()
         self.time_recoder = TimeRecode(self.writer)
+        self.nproc =nproc
+
+    def make_env(self, nproc=1):
+        if nproc <2:
+            env =  DummyVecEnv([lambda: gym.make(self.env_name)])
+        else:
+            env = make_vec_env (self.env_name, n_envs=nproc, vec_env_cls=SubprocVecEnv)
+        return VecNormalize(env, norm_obs=True, norm_reward=True, clip_obs=10.)
+
+
 
 
     def init_boost(self, min_profit=-500):
         print("-----  BOOST UP", self.name)
 
-        env = DummyVecEnv([lambda: gym.make(env_name)])
-        env = VecNormalize(env, norm_obs=True, norm_reward=True, clip_obs=10.)
+        env = self.make_env(self.nproc)
+        test_env =  DummyVecEnv([lambda: gym.make(self.env_name)])
 
         minimum = -1e8
         suit_model =None
         for iter in range(1,3):
             model = self._create(init=True, env=env)
+            self.train_start = time.time ()
             model.learn(total_timesteps= self.boost_step + iter*self.unit)
-            eval = self.evaluation(model, env)
+            eval = self.evaluation(model, test_env)
             profit = eval["1_Reward"]
-            print(" - - - - - BOOST PROFIT:   ",profit)
+
+            print(" - - - - - BOOST PROFIT: ", self.name, profit)
             if (minimum < profit):
                 minimum = profit
                 suit_model = model
@@ -115,7 +129,7 @@ class IterRun:
         try:
             model = self.model_cls.load(self.save, env=self.env)
             if self.buffer: model.replay_buffer = self.buffer
-            print("LOADED", self.save)
+            print("LOADED", self.save, self.iter)
             print("BUFFER REUSE:", model.replay_buffer.size())
         except:
             if self.boosted: raise Exception("INIT ERROR")
@@ -123,16 +137,18 @@ class IterRun:
 
         model.learn(total_timesteps=steps, tb_log_name=self.name)
         self.buffer = model.replay_buffer
-
+        fps = int(model._n_updates / (time.time()-self.train_start))
         model.save(self.save)
+
 
         train = {
             "1_Actor_loss": model.last_log["train/actor_loss"],
             "2_Critic_Loss": model.last_log["train/critic_loss"],
             "3_Reward": model.last_log["rollout/ep_rew_mean"],
-            "4_FPS": model.last_log["time/fps"]}
+            "4_FPS": fps}
 
-        eval = self.evaluation(model, self.env)
+        print("===========   EVAL   =======   ",self.name, self.iter, ",FPS: ", fps)
+        eval = self.evaluation(model, self.test_env)
         self.board("Eval", eval)
         self.board("Train",train)
         self.time_recoder.recode(eval)
@@ -144,15 +160,17 @@ class IterRun:
         for key, val in dict.items():
             self.writer.add_scalar(prefix + "/" + key, val, self.iter)
 
-    def evaluation(self, model, env):
-        env.training = False
-        env.norm_reward = False
+    def evaluation(self, model, env = None):
+        if not env: env = self.test_env
+        train_evn = model.get_env()
         obs = env.reset()
+        obs = train_evn.normalize_obs(obs)
         done = False
         rewards = []
         while not done:
             action, _states = model.predict(obs)
             obs, reward, done, info = env.step(action)
+            obs = train_evn.normalize_obs (obs)
             done = done[0]
             rewards.append(reward[0])
 

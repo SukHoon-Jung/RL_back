c52925d4e8cb1fad75c57654b166394b003f5c20
dreamersnme <34536053+dreamersnme@users.noreply.github.com>
2022-01-11 17:01:58 +0900

    Update dict_test.py

--- dict_test.py
+++ dict_test.py
@@ -2,6 +2,7 @@ import gym
 from gym.wrappers import FlattenObservation
 
 from runner.callbacks import LearnEndCallback
+from sim.env.Dict_env import DictEnv
 from sim.env.Dict_envtest import DictEnvTest
 import numpy as np
 from stable_baselines3 import DDPG, SAC,TD3
@@ -13,39 +14,64 @@ def consistency(model_class=TD3):
     Make sure that dict obs with vector only vs using flatten obs is equivalent.
     This ensures notable that the network architectures are the same.
     """
-    dict_env = DictEnvTest()
-    env = FlattenObservation(DictEnvTest())
 
-    dict_env =VecNormalize(DummyVecEnv([lambda : DictEnvTest()]), norm_obs_keys=["obs"])
-    env =VecNormalize(DummyVecEnv([lambda :FlattenObservation(DictEnvTest())]))
-
+    dict_env =VecNormalize(DummyVecEnv([lambda : DictEnv()]), norm_obs_keys=["stat", "obs"])
+    env = VecNormalize (DummyVecEnv ([lambda: FlattenObservation (DictEnv ())]))
+    env222 = VecNormalize(DummyVecEnv([lambda :FlattenObservation(DictEnvTest())]))
     dict_env.seed(10)
-    n_steps = 5
+    env.seed (10)
+    env222.seed (10)
+
+
+    n_steps = 2
     kwargs = dict(
         buffer_size=20000,
         gradient_steps=2,
-        batch_size =128
+        batch_size =1
     )
     CB1 = LearnEndCallback()
     CB2 = LearnEndCallback()
+    CB3 = LearnEndCallback ()
+
+
     dict_model = model_class("MultiInputPolicy", dict_env, gamma=0.99, seed=1,  **kwargs)
-    dict_model.learn(total_timesteps=n_steps, callback = CB1,)
-
-
+    # dict_model.learn(total_timesteps=n_steps, callback = CB1,)
 
     normal_model = model_class("MlpPolicy", env, gamma=0.99, seed=1, **kwargs)
     normal_model.learn(total_timesteps=n_steps, callback = CB2,)
 
-    print(CB1.last_aloss, CB2.last_aloss)
-    print(CB1.last_closs, CB2.last_closs)
+    normal_model2 = model_class ("MlpPolicy", env222, gamma=0.99, seed=1, **kwargs)
+    normal_model2.learn(total_timesteps=n_steps, callback=CB3, )
 
 
+
+
+    print(CB1.last_aloss, CB1.last_closs)
+    print (CB2.last_aloss, CB2.last_closs)
+    print (CB3.last_aloss, CB3.last_closs)
+
+    print (env.obs_rms.mean-env222.obs_rms.mean)
 
     obs = dict_env.reset()
-    for i in range(100):
+    o2 = env.reset ()
+    o3 = env222.reset ()
+    # #
+    # print(obs)
+    # print (o2)
+    # print (o3)
+    # print("==========")
+    # print(dict_env.unnormalize_obs(obs))
+    # print (env.unnormalize_obs (o2))
+    # print (env222.unnormalize_obs (o3))
+    # print("==========")
+    #
+
+    for i in range(5):
         action_1, _ = dict_model.predict(obs)
-        action_2, _ = normal_model.predict(obs["obs"])
-        print(action_1, "\t", action_2)
+        obs2 = obs["obs"].tolist()[0] + obs['stat'].tolist()[0]
+        action_2, _ = normal_model.predict(obs2)
+        action_22, _ = normal_model2.predict (obs2)
+        print(action_1, "\t", action_2, "\t", action_22)
         obs, reward, done, info  = dict_env.step(action_1)
 
 

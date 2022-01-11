ddc59233a98afcaa24aca716c06eb601d6efdf16
dreamersnme <34536053+dreamersnme@users.noreply.github.com>
2022-01-11 11:41:59 +0900

    Update torch_layers.py

--- stable_baselines3/common/torch_layers.py
+++ stable_baselines3/common/torch_layers.py
@@ -32,20 +32,7 @@ class BaseFeaturesExtractor(nn.Module):
         raise NotImplementedError()
 
 
-class FlattenExtractor(BaseFeaturesExtractor):
-    """
-    Feature extract that flatten the input.
-    Used as a placeholder when feature extraction is not needed.
 
-    :param observation_space:
-    """
-
-    def __init__(self, observation_space: gym.Space):
-        super(FlattenExtractor, self).__init__(observation_space, get_flattened_obs_dim(observation_space))
-        self.flatten = nn.Flatten()
-
-    def forward(self, observations: th.Tensor) -> th.Tensor:
-        return self.flatten(observations)
 
 
 class NatureCNN(BaseFeaturesExtractor):
@@ -234,6 +221,45 @@ class MlpExtractor(nn.Module):
         return self.value_net(self.shared_net(features))
 
 
+class FlattenExtractor(BaseFeaturesExtractor):
+    """
+    Feature extract that flatten the input.
+    Used as a placeholder when feature extraction is not needed.
+
+    :param observation_space:
+    """
+
+    def __init__(self, observation_space: gym.Space):
+        super(FlattenExtractor, self).__init__(observation_space, get_flattened_obs_dim(observation_space))
+        self.flatten = nn.Flatten()
+
+    def forward(self, observations: th.Tensor) -> th.Tensor:
+        return self.flatten(observations)
+
+# class CombinedExtractor(BaseFeaturesExtractor):
+#     """
+#     Combined feature extractor for Dict observation spaces.
+#     Builds a feature extractor for each key of the space. Input from each space
+#     is fed through a separate submodule (CNN or MLP, depending on input shape),
+#     the output features are concatenated and fed through additional MLP network ("combined").
+# 
+#     :param observation_space:
+#     :param cnn_output_dim: Number of features to output from each CNN submodule(s). Defaults to
+#         256 to avoid exploding network sizes.
+#     """
+#     key ='obs'
+# 
+#     def __init__(self, observation_space: gym.spaces.Dict):
+#         # TODO we do not know features-dim here before going over all the items, so put something there. This is dirty!
+#         super(CombinedExtractor, self).__init__(observation_space,  get_flattened_obs_dim(observation_space.spaces[self.key]))
+#         self.flatten = nn.Flatten()
+# 
+# 
+#     def forward(self, observations: TensorDict) -> th.Tensor:
+#         return self.flatten(observations[self.key])
+# 
+
+
 class CombinedExtractor(BaseFeaturesExtractor):
     """
     Combined feature extractor for Dict observation spaces.
@@ -246,7 +272,7 @@ class CombinedExtractor(BaseFeaturesExtractor):
         256 to avoid exploding network sizes.
     """
 
-    def __init__(self, observation_space: gym.spaces.Dict, cnn_output_dim: int = 256):
+    def __init__(self, observation_space: gym.spaces.Dict):
         # TODO we do not know features-dim here before going over all the items, so put something there. This is dirty!
         super(CombinedExtractor, self).__init__(observation_space, features_dim=1)
 
@@ -254,13 +280,10 @@ class CombinedExtractor(BaseFeaturesExtractor):
 
         total_concat_size = 0
         for key, subspace in observation_space.spaces.items():
-            if is_image_space(subspace):
-                extractors[key] = NatureCNN(subspace, features_dim=cnn_output_dim)
-                total_concat_size += cnn_output_dim
-            else:
-                # The observation key is a vector, flatten it if needed
-                extractors[key] = nn.Flatten()
-                total_concat_size += get_flattened_obs_dim(subspace)
+
+            # The observation key is a vector, flatten it if needed
+            extractors[key] = nn.Flatten()
+            total_concat_size += get_flattened_obs_dim(subspace)
 
         self.extractors = nn.ModuleDict(extractors)
 
@@ -289,6 +312,8 @@ def get_actor_critic_arch(net_arch: Union[List[int], Dict[str, List[int]]]) -> T
        If the number of ints is zero, the network will be linear.
     2. If it is a dict,  it should have the following structure:
        ``dict(qf=[<critic network architecture>], pi=[<actor network architecture>])``.
+
+
        where the network architecture is a list as described in 1.
 
     For example, to have actor and critic that share the same network architecture,

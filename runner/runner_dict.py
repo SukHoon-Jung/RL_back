12ab3bea42e0756bf18a229ca3ae53d5239453c8
dreamersnme <34536053+dreamersnme@users.noreply.github.com>
2022-01-11 21:16:45 +0900

    Update runner_dict.py

--- runner/runner_dict.py
+++ runner/runner_dict.py
@@ -10,35 +10,13 @@ from sim.env.Dict_env import DictEnv
 from sim.env.Dict_envtest import DictEnvTest
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.noise import NormalActionNoise
+from stable_baselines3.common.utils import set_random_seed
 from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize, SubprocVecEnv
 import numpy as np
 
 
 # <class 'stable_baselines3.td3.policies.TD3Policy'>
 # <class 'stable_baselines3.sac.policies.SACPolicy'>
-class TimeRecode:
-
-    def __init__(self, writer, interval=2):
-        self.total_sec=0
-        self.tick = 0
-        self.writer = writer
-        self.interval=interval
-
-    def start(self):
-        self.start_tm = time.time()
-
-    def recode(self, dict):
-        if dict is None: return False
-        self.total_sec += int(time.time()-self.start_tm)
-        now = (self.total_sec/60) / self.interval
-
-        if now >= self.tick:
-            for key, val in dict.items():
-                self.writer.add_scalar("TvIME" + "/" + key, val, now * self.interval)
-            self.tick = now + 1
-
-        return True
-
 
 
 ENV = DictEnv
@@ -46,11 +24,8 @@ class IterRun:
     MIN_TRADE = 100
     BOOST_SEARCH = 3
     unit = 2050
-    boost_step = 3* unit
-    boosted = False
-
     gradient_steps = 2
-    def __init__(self, MODEL, arc=[256, 128, 32], nproc=1, init_boost=True, retrain=False, batch_size=128, seed=None):
+    def __init__(self, MODEL, arc=[256, 128, 32], nproc=1, init_boost=False, retrain=False, batch_size=128, seed=None):
         self.seed = seed
         self.model_cls = MODEL
         self.name = MODEL.__name__
@@ -62,60 +37,57 @@ class IterRun:
         self.arch = arc
         self.iter = 1
 
+
         self.time_recoder = TimeRecode(self.writer)
         self.nproc =nproc
         self.batch_size = batch_size
         if retrain:
             pass
         else:
-            if init_boost: self.init_boost()
-            else: self.init_boost (-10_000)
+            if init_boost: self.init_boost(self.MIN_TRADE)
+            else: self.init_boost (30, -1e8)
 
     def make_env(self):
-        env =  DummyVecEnv([lambda: ENV(verbose=False)])
+        env = DummyVecEnv([lambda: ENV(verbose=False)])
         return VecNormalize(env, norm_obs_keys=["obs", "stat"])
 
-    def init_boost(self, min_reward=-500):
+    def init_boost(self, MIN_TRADE, min_reward=-500):
         print("-----  BOOST UP", self.name)
         env = self.make_env()
-        test_env =  ENV(())
+        test_env =  ENV()
 
         minimum = -1e8
         suit_model = None
-
         iter_cnt = 0
         while True:
-            self.seed = np.random.randint(100000)
-            model = self._create(env=env)
-
+            model = self._create(env=env, learning_starts = 200)
             self.train_start = time.time ()
-            model.learn(total_timesteps= self.boost_step + iter_cnt*self.unit)
+            learn_steps = self.unit *(3+iter_cnt)
+
+            model.learn(total_timesteps=learn_steps)
+            model.learning_starts = 0
             eval = self.evaluation(model, test_env)
             reward = eval["1_Reward"]
             count = eval['5_Trade']
             iter_cnt += 1
-            if count < self.MIN_TRADE:
-                
-                print (" - - - - - BOOST FAIL: ", self.name, reward, count)
+            if count < MIN_TRADE:
+                print (" - - - - - BOOST FAIL: ", self.name, reward, " by Count:", count)
                 continue
-
             print(" - - - - - BOOST PROFIT: ", self.name, reward)
+
             if (minimum < reward):
                 minimum = reward
                 suit_model = model
+            if reward > min_reward: break
+            if iter_cnt >= self.BOOST_SEARCH: break
 
-            if reward > min_reward:
-                break
-            if iter_cnt > self.BOOST_SEARCH: break
-
-        print (" - - - - - BOOST Selected: ", self.name, minimum, "Seed:", self.seed)
+        print (" - - - - - BOOST Selected: ", self.name, minimum, "Seed:", model.seed)
+        self.seed = model.seed
         self.buffer = suit_model.replay_buffer
         suit_model.learning_starts = 0
         suit_model.save(self.save)
-        self.boosted = True
+
         del suit_model
-
-
 
 
     def _create(self, env=None, learning_starts = 100 ):
@@ -125,7 +97,9 @@ class IterRun:
             mean=np.zeros(1), sigma=noise_std * np.ones(1)
         )
         if env is None:env = self.env
-        model = self.model_cls("MultiInputPolicy", env, verbose=1, action_noise=noise,seed = self.seed,
+        seed = self.seed or np.random.randint(1e8)
+        print(seed)
+        model = self.model_cls("MultiInputPolicy", env, verbose=1, action_noise=noise,
                                gradient_steps= self.gradient_steps,
                                batch_size = self.batch_size, policy_kwargs=policy_kwargs,
                                learning_starts=learning_starts)
@@ -136,17 +110,16 @@ class IterRun:
     def train_eval(self, steps =None):
 
         self.time_recoder.start()
-        if steps is None: steps = self.unit*3
+        self.seed = np.random.randint (1e8)
 
+        if steps is None: steps = self.unit*1
         model = self.model_cls.load(self.save, env=self.env)
-        print(model.seed)
-        if self.buffer: model.replay_buffer = self.buffer
+        model.replay_buffer = self.buffer
+        # model.set_random_seed (self.seed)
+        print (self.seed)
 
-        # print("LOADED", self.save, self.iter)
-        # print("BUFFER REUSE:", model.replay_buffer.size() * self.nproc)
-
-        start_tm = time.time()
-        start_n = model._n_updates
+        print("LOADED", self.save, self.iter, model.seed)
+        print("BUFFER REUSE:", model.replay_buffer.size() * self.nproc)
 
         CB = LearnEndCallback()
         model.learn(total_timesteps=steps, tb_log_name=self.name, callback=CB)
@@ -204,3 +177,29 @@ class IterRun:
         }
 
         return rslt
+
+
+
+class TimeRecode:
+
+    def __init__(self, writer, interval=2):
+        self.total_sec=0
+        self.tick = 0
+        self.writer = writer
+        self.interval=interval
+
+    def start(self):
+        self.start_tm = time.time()
+
+    def recode(self, dict):
+        if dict is None: return False
+        self.total_sec += int(time.time()-self.start_tm)
+        now = (self.total_sec/60) / self.interval
+
+        if now >= self.tick:
+            for key, val in dict.items():
+                self.writer.add_scalar("TvIME" + "/" + key, val, now * self.interval)
+            self.tick = now + 1
+
+        return True
+

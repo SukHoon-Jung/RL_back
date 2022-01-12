9b5af3990d328cf1692ee2a11cf860919194a563
dreamersnme <34536053+dreamersnme@users.noreply.github.com>
2022-01-12 21:51:46 +0900

    Update RUN.py

--- RUN.py
+++ RUN.py
@@ -1,53 +1,37 @@
+import sys
+
 import gym
 from gym import register
+
+from runner.runner_dict import IterRun
+from stable_baselines3 import DDPG, SAC,TD3
 import numpy as np
 
-from stable_baselines3 import DDPG, SAC
-from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.common.noise import NormalActionNoise
-from stable_baselines3.common.vec_env import SubprocVecEnv
+# https://github.com/notadamking/RLTrader/issues/10
 
 
-def make_env(env_id, nproc=2):
-    def _f(env_name, seed):
-        env = gym.make(env_name)
-        env.seed(seed)
-        return env
 
-    envs = [_f(env_id,seed) for seed in range(nproc)]
-    return SubprocVecEnv(envs)
+def compair_run(iter):
+    noise_set = np.linspace(0.05, 0.5,5)
+    nis_start = 10
 
-env_name ='StarTrader-v0'
-def run(model_name):
-
-    env = gym.make(env_name, title=model_name, plot_dir="./LOG_{}/figs".format(model_name))
-    # env = make_env(env_name)
-    # env= make_vec_env(model_name, n_envs=2, seed=0, vec_env_cls=SubprocVecEnv)
-
-    policy_kwargs = [256, 256]
-    policy_kwargs = dict(net_arch=policy_kwargs)
-    noise_std = 0.3
-    noise = NormalActionNoise(
-        mean=np.zeros(1), sigma=noise_std * np.ones(1)
-    )
-    tensorboard_log="./summary/"
-
-    if model_name =="DDPG":
-        model = DDPG("MlpPolicy", env, verbose=1, action_noise=noise ,gradient_steps=2,
-                     policy_kwargs = policy_kwargs, tensorboard_log=tensorboard_log)
-    else:
-        model = SAC("MlpPolicy", env, verbose=1, action_noise=noise ,
-                     policy_kwargs = policy_kwargs, tensorboard_log=tensorboard_log)
-
-    model.learn(total_timesteps=1e8, tb_log_name = model_name)
-
-
+    if model =="TD3": targets =[IterRun(TD3)]
+    elif model =="DDPG": targets =[IterRun(DDPG)]
+    elif model =="SAC": targets =[IterRun(SAC)]
+    else: pass
+    print("======================================")
+    print ("======================================")
+    print ("======================================")
+    for i in range(1, iter):
+        noise = None if i <nis_start else noise_set[(i-nis_start) % len(noise_set)]
+        for iter_run in targets:
+            iter_run.train_eval(noise=noise)
 
 
 
 if __name__ == '__main__':
-    register(
-        id=env_name,
-        entry_point='sim.env:StarTradingEnv',
-    )
-    run("DDPG")
+    model = sys.argv[-1]
+    print(model)
+    compair_run(1000, model)
+
+

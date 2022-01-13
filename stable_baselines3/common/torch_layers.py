9ed5c62c6b105640f1f1f39355e6747e6d7f1f6b
dreamersnme <34536053+dreamersnme@users.noreply.github.com>
2022-01-13 21:02:25 +0900

    Update torch_layers.py

--- stable_baselines3/common/torch_layers.py
+++ stable_baselines3/common/torch_layers.py
@@ -240,11 +240,10 @@ class FlattenExtractor(BaseFeaturesExtractor):
 
 class SeqCNN (BaseFeaturesExtractor):
     ch1 = 3
-    def __init__(self, observation_space: gym.spaces.Box, span=2, cnn_dim: int = 64):
-        super (SeqCNN, self).__init__ (observation_space, features_dim=cnn_dim)
-
+    def __init__(self, observation_space: gym.spaces.Box, span=2, outdim: int = 64):
+        super (SeqCNN, self).__init__ (observation_space, features_dim=outdim)
         seq_len = observation_space.shape[0]
-        seq_width =  observation_space.shape[1]
+        seq_width = observation_space.shape[1]
         span = min (seq_len, span)
         n_cnn = (seq_len - span +1) * seq_width * self.ch1
 
@@ -253,12 +252,14 @@ class SeqCNN (BaseFeaturesExtractor):
             nn.Conv2d (1, self.ch1, kernel_size=(span, 1), stride=1),
             nn.ReLU(),
             nn.Flatten (),
-            nn.Linear (n_cnn, cnn_dim),
+            nn.Linear (n_cnn, outdim),
             nn.ReLU ()
         )
 
     def forward(self, observations: th.Tensor) -> th.Tensor:
         return self.cnn(observations)
+
+
 
 class SeqLast(BaseFeaturesExtractor):
     def __init__(self, observation_space: gym.Space):
@@ -267,28 +268,61 @@ class SeqLast(BaseFeaturesExtractor):
     def forward(self, observations: th.Tensor) -> th.Tensor:
         return self.flatten (observations[:,-1])
 
-class SeqLstm (BaseFeaturesExtractor):
-    def __init__(self, observation_space: gym.spaces.Box, out_dim: int = 64):
-        super (SeqLstm, self).__init__ (observation_space, features_dim=out_dim)
-        seq_width = observation_space.shape[1]
-        hidden = int( (2+seq_width)/2)
+class LstmLast(nn.Module):
+    def __init__(self, seq_width, hidden):
+        super (LstmLast, self).__init__ ()
         self.lstm = nn.LSTM (input_size=seq_width, hidden_size=hidden, batch_first=True)
-        self.out = nn.Sequential(
+    def forward(self, x):
+        out, _ = self.lstm(x)
+        return out[:,-1]
+
+
+
+class SeqCRNN (BaseFeaturesExtractor):
+    ch1 = 3
+    def __init__(self, observation_space: gym.spaces.Box, span=3, outdim: int = 128):
+        super (SeqCRNN, self).__init__ (observation_space, features_dim=outdim)
+        seq_len = observation_space.shape[0]
+        seq_width = observation_space.shape[1]
+        span = min (seq_len, span)
+        out_len = (seq_len - span +1)
+        assert out_len > 1
+
+        self.crnn = nn.Sequential (
+            nn.Unflatten (-2, (1, seq_len)),
+            nn.Conv2d (1, self.ch1, kernel_size=(span, 1)),
             nn.ReLU (),
-            nn.Flatten(),
-            nn.Linear(hidden, out_dim),
+            nn.Conv2d (self.ch1, 1, kernel_size=(1, 1)),
+            nn.Flatten (end_dim=-2),
+            LstmLast (seq_width=seq_width, hidden=outdim),
             nn.ReLU ()
         )
 
     def forward(self, observations: th.Tensor) -> th.Tensor:
-        out,_ = self.lstm (observations)
-        out = out[:,-1]
-        return self.out(out)
+        return self.crnn(observations)
+
+
+class SeqLstm (BaseFeaturesExtractor):
+    def __init__(self, observation_space: gym.spaces.Box, out_dim: int = 128):
+        super (SeqLstm, self).__init__ (observation_space, features_dim=out_dim)
+        seq_width = observation_space.shape[1]
+        # self.lstm = LstmLast(seq_width=seq_width, hidden=hidden)
+        self.lstm = nn.Sequential(
+            LstmLast (seq_width=seq_width, hidden=out_dim),
+            nn.ReLU ()
+        )
+
+    def forward(self, observations: th.Tensor) -> th.Tensor:
+        return self.lstm(observations)
+
+
+
+
 
 class SeqFeature(BaseFeaturesExtractor):
     def __init__(self, obs_space: gym.spaces.Box):
         super (SeqFeature, self).__init__ (obs_space, features_dim=1)
-        extractors =[SeqLast(obs_space), SeqCNN(obs_space), SeqLstm(obs_space)]
+        extractors =[ SeqCRNN(obs_space), SeqLstm(obs_space)]
         total_concat_size = 0
         for ex in extractors:
             total_concat_size += ex._features_dim
@@ -299,7 +333,7 @@ class SeqFeature(BaseFeaturesExtractor):
     def forward(self, obs: th.Tensor) -> th.Tensor:
         encoded_tensor_list = []
         for ext in self.extractors:
-            encoded_tensor_list.append( ext(obs))
+            encoded_tensor_list.append(ext(obs))
         return th.cat(encoded_tensor_list, dim=1)
 
 
@@ -313,7 +347,7 @@ class CombinedExtractor(BaseFeaturesExtractor):
 
         for key, subspace in observation_space.spaces.items():
             if key ==OBS:
-                extractors[OBS] = SeqFeature(subspace)
+                extractors[OBS] =SeqLstm(subspace) # SeqFeature(subspace)
                 total_concat_size += extractors[OBS]._features_dim
 
             elif key == STAT:
